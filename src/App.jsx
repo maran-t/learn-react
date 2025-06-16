92db72c63af013baea0e0929e6e4fcea460dee68
Manimaran T <maran.mallow@gmail.com>
2025-06-16 17:58:28 +0530

    init: clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,8 @@
-import Login from './components/Login';
-import { ThemeProvider } from './context/theme';
-import UserContextProvider from './context/UserContextProvider';
-import ThemeBtn from './components/ToggleBtn';
 import './App.css';
 
 function App() {
   return (
-    <div className='dark:bg-black'>
-      <ThemeProvider>
-        <UserContextProvider>
-          <ThemeBtn />
-          <Login />
-        </UserContextProvider>
-      </ThemeProvider>
-    </div>
+    <div> hello, world! </div>
   )
 }
 
