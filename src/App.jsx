b2ba9a09a346eead959104851c7afe526ee74920
Manimaran T <maran.mallow@gmail.com>
2025-06-22 16:30:59 +0530

    basic: other way to impl context api of basics05

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,6 @@ import { TodoForm } from './components/TodoForm';
 import { TodoItem } from './components/TodoItem';
 
 function App() {
-  // const {todos} = useTodo();
-
-  // useEffect(() => {
-  //   const todos = JSON.parse(localStorage.getItem('todos')) || [];
-  //   if (todos && todos.length) {
-  //     setTodos(todos);
-  //   }
-  // }, []);
-
-  function abcd() {
-
-  }
-
-
-
   return (
     <TodoProvider>
       <TodoForm>
